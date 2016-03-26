e33747ae6f1dce727cd157d43fc624afab41e005
Raleigh Matlock <raleighman2@gmail.com>
2016-03-26 13:23:57 -0400

    Turbo-ify

--- turbo.mk
+++ turbo.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Turbo stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := turbo_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
